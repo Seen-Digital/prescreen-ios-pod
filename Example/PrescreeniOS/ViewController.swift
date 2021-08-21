2499268da43105b64d1f047c07f75f657a9cfad3
Thanapon Noraset <nor.thanapon@gmail.com>
2021-08-21 11:44:43 +0700

    Release 0.0.2.

--- Example/PrescreeniOS/ViewController.swift
+++ Example/PrescreeniOS/ViewController.swift
@@ -2,23 +2,97 @@
 //  ViewController.swift
 //  PrescreeniOS
 //
-//  Created by northanapon on 08/21/2021.
-//  Copyright (c) 2021 northanapon. All rights reserved.
+//  Created by
+//      - Verachad Wongsawangtham
+//      - Thanapon Noraset
+//  Copyright (c) 2021 Seen Digital. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
+import PrescreeniOS
 
 class ViewController: UIViewController {
 
+    var backFacingCamera: AVCaptureDevice?
+    var frontFacingCamera: AVCaptureDevice?
+    var currentDevice: AVCaptureDevice!
+    
+    var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
+    
+    let captureSession = AVCaptureSession()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Prescreen.shareInstance.initialize(apiKey: "68OvRGckCYiElYRqMqv7")
+        configure()
     }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        captureSession.stopRunning()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - Helper methods
+    private func configure() {
+        captureSession.sessionPreset = AVCaptureSession.Preset.high
+        
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInTelephotoCamera,.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .unspecified)
+        
+        for device in deviceDiscoverySession.devices {
+            if device.position == .back {
+                backFacingCamera = device
+            } else if device.position == .front {
+                frontFacingCamera = device
+            }
+        }
+        
+        currentDevice = backFacingCamera
+        
+        guard let captureDeviceInput = try? AVCaptureDeviceInput(device: currentDevice) else {
+            return
+        }
+
+        let output = AVCaptureVideoDataOutput()
+        let queue = DispatchQueue(label: "myqueue")
+        output.alwaysDiscardsLateVideoFrames = true
+        output.setSampleBufferDelegate(self, queue: queue)
+        
+        captureSession.addInput(captureDeviceInput)
+        captureSession.addOutput(output)
+        
+        cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(cameraPreviewLayer!)
+        cameraPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        cameraPreviewLayer?.frame = view.layer.frame
+        
+
+        captureSession.startRunning()
+        
+    }
 }
+
+extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        Prescreen.shareInstance.scanIDCard(sampleBuffer: sampleBuffer, cameraPosition: self.currentDevice.position) { result in
+            if result.error != nil {
+                print(result.error!)
+            }
+            if result.texts != nil {
+                print("OCR: \(result.confidence)")
+                print(result.texts!)
+            }
+            if result.detectionResult != nil {
+                print("ML: \(result.detectionResult!.mlConfidence), BOX: \(result.detectionResult!.boxConfidence)")
+            }
+            // Handle result here
+        }
+    }
+}
+
 
--- PrescreeniOS.podspec
+++ PrescreeniOS.podspec
@@ -8,35 +8,31 @@
 
 Pod::Spec.new do |s|
   s.name             = 'PrescreeniOS'
-  s.version          = '0.1.0'
-  s.summary          = 'A short description of PrescreeniOS.'
-
-# This description is used to generate tags and improve search results.
-#   * Think: What does it do? Why did you write it? What is the focus?
-#   * Try to keep it short, snappy and to the point.
-#   * Write the description between the DESC delimiters below.
-#   * Finally, don't worry about the indent, CocoaPods strips it!
-
+  s.version          = '0.0.2'
+  s.summary          = 'PrescreeniOS detect and verify Thai national ID cards.'
   s.description      = <<-DESC
-TODO: Add long description of the pod here.
+  'PrescreeniOS detect and verify Thai national ID cards. It works for both realtime input (camera feed) or an image.'
                        DESC
 
-  s.homepage         = 'https://github.com/northanapon/PrescreeniOS'
+  s.homepage         = 'https://github.com/InDistinct-Studio/prescreen-ios-pod.git'
   # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
   s.author           = { 'northanapon' => 'nor.thanapon@gmail.com' }
-  s.source           = { :git => 'https://github.com/northanapon/PrescreeniOS.git', :tag => s.version.to_s }
+  s.source           = { :http => 'https://github.com/InDistinct-Studio/prescreen-ios-pod/releases/download/v0.0.1-alpha/PrescreeniOS.zip' }
+#  s.source           = { :git => 'https://github.com/InDistinct-Studio/prescreen-ios-pod.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
-  s.ios.deployment_target = '9.0'
-
-  s.source_files = 'PrescreeniOS/Classes/**/*'
-  
+  s.ios.deployment_target = '12.1'
+  s.ios.vendored_frameworks = 'PrescreeniOS.framework'
   # s.resource_bundles = {
   #   'PrescreeniOS' => ['PrescreeniOS/Assets/*.png']
   # }
+  s.resources = 'PrescreeniOS.bundle'
 
   # s.public_header_files = 'Pod/Classes/**/*.h'
   # s.frameworks = 'UIKit', 'MapKit'
   # s.dependency 'AFNetworking', '~> 2.3'
+  s.static_framework = true
+  s.dependency 'GoogleMLKit/TextRecognition'
+  s.dependency 'GoogleMLKit/ObjectDetectionCustom'
 end
